c3001a678e6d325aa121c892e2b1f6eeea1fce43
Douglas Gardner <douglas@chippy.ch>
2020-07-26 05:36:08 +0100

    Handle weirdly formed transcripts a bit better

--- routes/index.js
+++ routes/index.js
@@ -22,15 +22,25 @@ router.get('/comic/:index', function (req, res, next) {
   const synthetic = fileName.includes('gpt')
   const comic = fs.readFileSync(path.join(dir, fileName), 'utf8')
   const panels = comic.split('\n\n')
-  const parsedPanels = panels.map((panel) => {                                 
-    const lines = panel.split('\n')                                            
-    return lines.map((line) => {                                               
-      const x = line.split(': ', 2)    
-      const speaker = slug(x[0])                                      
-      return {speaker, text: x[1]}         
-    })                                                                         
-  }) 
-  //res.json(comic)     
+  const parsedPanels = panels.map((panel) => {
+    const lines = panel.split('\n')
+    return lines.map((line) => {
+      if (line.startsWith('{{')) return null
+      const x = line.split(': ')
+      if (x.length === 1) {
+        if (line && line == line.toUpperCase()) {
+          return {speaker: slug('Narrator'), text: line}
+        } else {
+          return null
+        }
+      }
+      const speaker = slug(x[0])
+      const speech = x.splice(1).join(': ');
+      if (!speech) return null
+      return {speaker, text: speech}
+    })
+  })
+  //res.json(comic)
   res.render('comic', {comic: parsedPanels, original: comic, fileName, synthetic})
 })
 
